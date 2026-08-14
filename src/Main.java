a7d4db02a3f01f3c729bef6e0150872ae68d7433
Rayssa Ketlen de Freitas Carvalho <rayssa.11881584@aluno.mg.gov.br>
2026-08-14 09:47:36 -0300

    feat: adicionar arrays de candidatos e matriz de votos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,135 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
+import java.util.Scanner;
 
+ class SistemaVotacao {
+
+    static Scanner scanner = new Scanner(System.in);
+    static void main(String[] args) {
+        System.out.println("Sistema de votação iniciado.");
+        scanner.close();
+        final int MAX_CANDIDATOS = 5;
+         final int TOTAL_TURMAS = 3;
+         final int MAX_VOTANTES_POR_TURMA = 10;
+         int[] numerosCandidatos = new int[MAX_CANDIDATOS];
+        String[] nomesCandidatos = new String[MAX_CANDIDATOS];
+         int[] votosCandidatos = new int[MAX_CANDIDATOS];
+        int[][] votosPorTurma = new int[TOTAL_TURMAS][MAX_VOTANTES_POR_TURMA];
+        int[] quantidadeVotosTurma = new int[TOTAL_TURMAS];
+         int quantidadeCandidatos = 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 }
+
